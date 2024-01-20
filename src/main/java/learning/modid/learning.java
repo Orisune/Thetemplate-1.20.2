71451efe1681060d5ebee3ffe7f63cfefe5cc023
Orisune <33106588+Orisune@users.noreply.github.com>
2024-01-19 16:54:22 -0800

    new item, and name fix

---
+++ src/main/java/learning/modid/learning.java
@@ -0,0 +1,15 @@
+package learning.modid;
+import learning.modid.Item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class Learning implements ModInitializer {
+	public static String MOD_ID = "learning";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitialize()
+	{
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/learning/modid/learningDataGenerator.java
@@ -0,0 +1,11 @@
+package learning.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LearningDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
